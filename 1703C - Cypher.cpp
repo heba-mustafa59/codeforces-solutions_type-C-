19fe71b133eca27a31c02320584dddba753cc2bc
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-01 19:51:28 +0300

    1703C - Cypher

---
+++ 1703C - Cypher.cpp
@@ -0,0 +1,41 @@
+﻿/*
+      yildiz
+*/
+#include <string>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            int b;
+            string s;
+            cin >> b >> s;
+            for (int j = 0; j < s.length(); j++) {
+                char m = s[j];
+                if (m == 'U') {
+                    v[i] = (v[i] == 0 ? 9 : v[i] - 1);
+                }
+                else {
+                    v[i] = (v[i] == 9 ? 0 : v[i] + 1);
+                }
+            }
+        }
+        for (int x : v) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+}
