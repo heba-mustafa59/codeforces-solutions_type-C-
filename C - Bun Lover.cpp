8b005a3fbf318fb9a3e0af013f1d3c1d241d312f
𝐇𝐞𝐛𝐚 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-17 17:17:16 +0300

    C - Bun Lover

---
+++ C - Bun Lover.cpp
@@ -0,0 +1,18 @@
+﻿/*
+      yildiz
+*/
+#define ll long long
+#define habboosh main
+#include <bits/stdc++.h>
+using namespace std;
+
+int habboosh() {
+    ll t; cin >> t;
+    while (t--) {
+        ll n; 
+        cin >> n;
+        n++;
+        cout << (n * n) + 1 <<  "\n";
+    }
+    return 0;
+}
